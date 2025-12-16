22330adf201e461328201b6a62a5777b9561e8c8
🍬 𝙹𝙾𝙻𝙻𝚈 𝙽𝚂 <159073116+1056516@users.noreply.github.com>
2025-12-16 18:35:46 -0500

    Delete active directory

--- active/uv/uv.config.js
+++
@@ -1,7 +0,0 @@
-self.__uv$config = {
-    prefix: '/active/go/',
-    bare:'https://mkykg-github-io.vercel.app/ub/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/active/uv/uv.handler.js',
-}
